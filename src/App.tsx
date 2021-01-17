6b969bb9c919021812a3613703bd1a8e107fdfeb
Hoang Le <kuhpro@gmail.com>
2021-01-17 14:55:01 -0600

    Add protected and public route and redirect user based on authentication state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import DynamicRoute from './components/DynamicRoute';
 import AuthProvider from './contexts/AuthProvider';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -10,9 +11,15 @@ const App = (): ReactElement => {
     <AuthProvider>
       <Router>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={Signup} />
+          <DynamicRoute
+            exact
+            path='/'
+            component={Home}
+            shouldAuthenticate
+            redirectTo='/login'
+          />
+          <DynamicRoute path='/login' component={Login} redirectTo='/' />
+          <DynamicRoute path='/signup' component={Signup} redirectTo='/' />
         </Switch>
       </Router>
     </AuthProvider>
--- src/components/DynamicRoute.tsx
+++ src/components/DynamicRoute.tsx
@@ -1,7 +1,41 @@
-import { ReactElement } from 'react';
+import { ComponentType, ReactElement, useContext } from 'react';
+import {
+  RouteProps,
+  Route,
+  Redirect,
+  RouteComponentProps,
+} from 'react-router-dom';
+import { AuthStateContext } from '../contexts/AuthProvider';
 
-const DynamicRoute = (): ReactElement => {
-  return <div></div>;
+interface DynamicRouteProps extends RouteProps {
+  component: ComponentType<RouteComponentProps<any>>;
+  shouldAuthenticate?: boolean;
+  redirectTo: string;
+}
+
+const DynamicRoute = ({
+  component: Component,
+  shouldAuthenticate,
+  redirectTo,
+  ...otherRouteProps
+}: DynamicRouteProps): ReactElement => {
+  const { authenticated } = useContext(AuthStateContext);
+
+  return (
+    <Route
+      {...otherRouteProps}
+      render={props => {
+        if (
+          (shouldAuthenticate && authenticated) ||
+          (!shouldAuthenticate && !authenticated)
+        ) {
+          return <Component {...props} />;
+        } else {
+          return <Redirect to={redirectTo} />;
+        }
+      }}
+    />
+  );
 };
 
 export default DynamicRoute;
