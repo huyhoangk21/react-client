ffd8fcdc7219bfe816d3a312af0639ca6113f337
Hoang Le <kuhpro@gmail.com>
2021-01-16 18:59:58 -0600

    Add axios to make requests to back end

--- src/validations/LoginSchema.tsx
+++ src/validations/LoginSchema.tsx
@@ -1,7 +1,9 @@
 import * as Yup from 'yup';
 
 const LoginSchema = Yup.object().shape({
-  email: Yup.string().required('Email must not be empty'),
+  email: Yup.string()
+    .email('Email must be valid')
+    .required('Email must not be empty'),
   password: Yup.string().required('Password must not be empty'),
 });
 
